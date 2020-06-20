7c98c1d835273aa4d4949b945ea53b7f6bb9020b
Susanta <inbox.susanta@gmail.com>
2020-06-20 21:21:48 +0530

    Legends

--- graph.js
+++ graph.js
@@ -33,12 +33,29 @@ const arcPath = d3
 // ordinal scale
 const colour = d3.scaleOrdinal(d3['schemeSet3']);
 
+// legend setup
+const legendGroup = svg
+  .append('g')
+  .attr('transform', `translate(${dims.width + 40}, 10)`);
+
+// custom path for legend
+// const legend = d3.legendColor()
+//   .shape('path', d3.symbol().type(d3.symbolCircle)())
+//   .shapePadding(10)
+//   .scale(colour)
+
+const legend = d3.legendColor().shape('circle').shapePadding(10).scale(colour);
+
 // update function
 const update = (data) => {
   // console.log(data);
 
   // update colour scale domain
   colour.domain(data.map((d) => d.name));
+
+  //   update ans call legend
+  legendGroup.call(legend);
+  legendGroup.selectAll('text').attr('fill', 'white');
 
   // join enhanced (pie) data to path elements
   const paths = graph.selectAll('path').data(pie(data));
